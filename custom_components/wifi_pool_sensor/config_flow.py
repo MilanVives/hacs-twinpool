b5ef7dde6670d459b0abf1648031799bc812b87f
flipkill1985 <jan.mohs.1985@gmail.com>
2024-08-20 15:52:14 +0200

    test
    test

--- custom_components/wifi_pool_sensor/config_flow.py
+++ custom_components/wifi_pool_sensor/config_flow.py
@@ -11,15 +11,17 @@ class WifiPoolSensorConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         if user_input is not None:
             return self.async_create_entry(title="WiFi Pool Sensor", data=user_input)
 
+        data_schema = vol.Schema({
+            vol.Required("email", description={"suggested_value": user_input.get("email") if user_input else ""}): str,
+            vol.Required("password", description={"suggested_value": user_input.get("password") if user_input else ""}): str,
+            vol.Required("domain", description={"suggested_value": user_input.get("domain") if user_input else ""}): str,
+            vol.Optional("io", description={"suggested_value": user_input.get("io") if user_input else ""}): str,
+            vol.Optional("io_flow", description={"suggested_value": user_input.get("io_flow") if user_input else ""}): str,
+            vol.Optional("io_redox", description={"suggested_value": user_input.get("io_redox") if user_input else ""}): str
+        })
+
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema({
-                vol.Required("email"): str,
-                vol.Required("password"): str,
-                vol.Required("domain"): str,
-                vol.Required("io"): str,
-                vol.Required("io_flow"): str,
-                vol.Required("io_redox"): str,
-            }),
+            data_schema=data_schema,
             errors=errors
         )
