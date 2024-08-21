e352bc9bb6b23217bb765b2d33075a32ae5e616e
flipkill1985 <jan.mohs.1985@gmail.com>
2024-08-21 09:57:23 +0200

    Version 1.0
    Version 1.0

--- custom_components/wifi_pool_sensor/config_flow.py
+++ custom_components/wifi_pool_sensor/config_flow.py
@@ -1,27 +1,29 @@
-async def async_step_user(self, user_input=None):
-    errors = {}
-    if user_input is not None:
-        return self.async_create_entry(title="WiFi Pool Sensor", data=user_input)
+import voluptuous as vol
+from homeassistant import config_entries
+from homeassistant.core import callback
+from .const import DOMAIN
 
-    data_schema = vol.Schema({
-        vol.Required("email"): str,
-        vol.Required("password"): str,
-        vol.Required("domain"): str,
-        vol.Optional("io"): str,
-        vol.Optional("io_flow"): str,
-        vol.Optional("io_redox"): str
-    })
+@callback
+def configured_instances(hass):
+    """Return a set of configured instances."""
+    return {entry.title for entry in hass.config_entries.async_entries(DOMAIN)}
 
-    return self.async_show_form(
-        step_id="user",
-        data_schema=data_schema,
-        errors=errors,
-        description_placeholders={
-            "email": "your email",
-            "password": "your password",
-            "domain": "your domain ID",
-            "io": "sensor ID for pH",
-            "io_flow": "sensor ID for flow",
-            "io_redox": "sensor ID for redox"
-        }
-    )
+class WifiPoolConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for WiFi Pool."""
+
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        """Handle the initial step."""
+        errors = {}
+
+        if user_input is not None:
+            return self.async_create_entry(title=user_input["email"], data=user_input)
+
+        data_schema = vol.Schema({
+            vol.Required("email"): str,
+            vol.Required("password"): str,
+            vol.Required("domain"): str,
+        })
+
+        return self.async_show_form(step_id="user", data_schema=data_schema, errors=errors)
